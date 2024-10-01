846d3f26147207b65bb2adc839fd0df9a3f134e8
mosy2266 <mosy2266@naver.com>
2024-10-01 20:13:08 +0900

    initial commit

---
+++ glow_refactoring/src/main/java/com/uhm/glow_refactoring/GlowRefactoringApplication.java
@@ -0,0 +1,13 @@
+package com.uhm.glow_refactoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlowRefactoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlowRefactoringApplication.class, args);
+	}
+
+}
---
+++ glow_refactoring/src/test/java/com/uhm/glow_refactoring/GlowRefactoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uhm.glow_refactoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlowRefactoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
